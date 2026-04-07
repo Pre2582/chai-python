463a19abfc22aaf481e1dbf133e5bdcf788e8b41
Pre2582 <premkumar38401@gmail.com>
2026-04-07 09:38:36 +0530

    Add cheatsheets for Python virtual environments and conda; include examples for environment management

--- 01_basics/hello_chai.py
+++ 01_basics/hello_chai.py
@@ -8,3 +8,31 @@ chai("leamon tea")
 chai_one = "leamon tea"
 chai_two = "ginger tea"
 chai_three = "masala chai"
+
+
+import copy
+
+chai = [[chai_one, chai_two, chai_three]]
+
+chai_copy = copy.deepcopy(chai)
+print(chai)
+print(chai_copy)
+chai_copy[0][0] = "green tea"
+print(chai)
+print(chai_copy)
+chai_shallow_copy = copy.copy(chai)
+chai_shallow_copy[0][0] = "black tea"
+print(chai)
+print(chai_shallow_copy)
+
+
+a =[[1,2,3],[4,5,6]]
+
+b = copy.deepcopy(a)  #deep copy
+c = copy.copy(a)  #shallow copy
+
+a[0][0] = 999
+print(a)
+print(b)
+print(c)
+
